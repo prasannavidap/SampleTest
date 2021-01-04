147e49cbc934563260cc40411e3b0bc368f6242b
prasanna <prasannavidap25@gmail.com>
2021-01-04 12:09:27 +0530

    Changes Committed

--- api-testing-poc/src/test/java/ServicesApiTest.java
+++ api-testing-poc/src/test/java/ServicesApiTest.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class ServicesApiTest {
 
     static String apiUrl;
-
+    //This is the annotation
     @BeforeAll
     static void getApiUrl() {
         apiUrl = System.getProperty("apiUrl");
